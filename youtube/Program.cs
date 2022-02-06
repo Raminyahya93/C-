8163c7b0dffac04e026406a60d2e472737f17f95
RaminYahya <95285624+Raminyahya93@users.noreply.github.com>
2022-02-06 12:19:13 +0330

    Add files via upload

---
+++ youtube/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace youtube
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = Console.ReadLine(); 
+            Console.WriteLine("hello " + x + "!");
+            Console.WriteLine($"hello {x}!");
+            Console.WriteLine("yo");
+
+        }
+    }
+}
